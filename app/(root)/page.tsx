6bbf3885a53b9c514e81e3884d172725a0effb6e
Devam Barbahya <barbhaya.devam@gmail.com>
2024-07-02 10:07:56 +0530

    Authentication

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,10 @@
+import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main>
-      <h1>Threads</h1>
+      <UserButton afterSignOutUrl="/" />
     </main>
   );
 }
