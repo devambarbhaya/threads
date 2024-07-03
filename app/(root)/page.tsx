a1785495b724c98ee68f6a9254f06ffc87d32efb
Devam Barbahya <barbhaya.devam@gmail.com>
2024-07-03 17:43:15 +0530

    Home Page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>
-      <UserButton afterSignOutUrl="/" />
-    </main>
+    <>
+      <h1 className="head-text text-left">Home</h1>
+    </>
   );
 }
